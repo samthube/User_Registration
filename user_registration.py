2b089b780ba980514945016801ab3388979844bf
Sam Thube <samadhanthube444@gmail.com>
2024-09-09 14:44:11 +0530

    [Samadhan] Add: Program to validate first name

---
+++ user_registration.py
@@ -0,0 +1,35 @@
+'''
+@Author: Samadhan Thube
+@Date: 2024-09-09
+@Last Modified by: Samadhan Thube
+@Last Modified time: 2024-09-09
+@Title : User Registration Problem
+'''
+
+import re
+
+def validate_first_name(first_name):
+    """
+    Description:
+        Validates if the first name starts with a capital letter and has at least 3 characters using regex.
+    
+    Parameter:
+        first_name: The first name entered by the user.
+    
+    Return:
+        bool: True if the first name is valid, False otherwise.
+    """
+    pattern = r'^[A-Z][a-zA-Z]{2,}$'
+    if re.match(pattern, first_name):
+        return True
+    return False
+
+def main():
+    first_name = input("Enter your first name: ")
+    if validate_first_name(first_name):
+        print("First name is valid.")
+    else:
+        print("Invalid first name. Please ensure it starts with a capital letter and is at least 3 characters long.")
+
+if __name__ == "__main__":
+    main()
