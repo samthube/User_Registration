47af22aa0c66b3708771e6d305dc548635b24c56
Sam Thube <samadhanthube444@gmail.com>
2024-09-12 22:11:18 +0530

    [Samadhan] Add : Logger file

---
+++ mylogging.py
@@ -0,0 +1,19 @@
+import logging
+
+def logger_init(name):
+    logger = logging.getLogger(name)
+    
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+    file_handler = logging.FileHandler('all.log')
+   
+    file_handler.setFormatter(formatter)
+   
+    stream_handler = logging.StreamHandler()
+    
+    stream_handler.setFormatter(formatter)
+ 
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    return logger    
